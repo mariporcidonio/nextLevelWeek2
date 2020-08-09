112b9b1aa5e791341fa1b19137bc2dbe422832e1
Marielle Porcidonio <mariporcidonio@hotmail.com>
2020-08-09 20:18:58 -0300

    Incluído addField.js

---
+++ scripts/addField.js
@@ -0,0 +1,10 @@
+//Procurar o botão
+document.querySelector("#add-time")
+//Quando clicar no botão 
+.addEventListener('click', cloneField)
+//Executar uma ação
+function cloneField() {
+    //Duplicar os campos
+
+    //Colocar na página
+}
