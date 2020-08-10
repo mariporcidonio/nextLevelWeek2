7cd490ff7636d5bcc3b4ee03cf046370dc4c6af9
Marielle Porcidonio <mariporcidonio@hotmail.com>
2020-08-09 21:18:59 -0300

    Finalização addField.js

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,10 +1,17 @@
 //Procurar o botão
 document.querySelector("#add-time")
 //Quando clicar no botão 
-.addEventListener('click', cloneField)
+.addEventListener('click', cloneField);
 //Executar uma ação
 function cloneField() {
-    //Duplicar os campos
-
-    //Colocar na página
+    //Duplicar os campos. Que campo?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+    //Pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input');
+    //Pega o field do momento e limpar
+    fields.forEach(function(field) {
+        field.value = "";
+    });
+    //Colocar na página. Onde??
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
 }
